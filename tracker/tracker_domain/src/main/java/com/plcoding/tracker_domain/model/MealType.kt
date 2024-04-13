783d736ea7481c41b959153cde836f2e8f47be65
Darya Kovalchuk <daryakovalchuk167@gmail.com>
2024-04-13 23:07:19 +0300

    add TrackerOverviewViewModel

--- tracker/tracker_domain/src/main/java/com/plcoding/tracker_domain/model/MealType.kt
+++ tracker/tracker_domain/src/main/java/com/plcoding/tracker_domain/model/MealType.kt
@@ -1,7 +1,7 @@
 package com.plcoding.tracker_domain.model
 
 sealed class MealType(val name: String) {
-    object Breakfact: MealType("breakfast")
+    object Breakfast: MealType("breakfast")
     object Lunch: MealType("lunch")
     object Dinner: MealType("dinner")
     object Snack: MealType("snack")
@@ -9,10 +9,10 @@ sealed class MealType(val name: String) {
     companion object{
         fun fromString(name:String):MealType{
             return when(name){
-                "breakfast" -> Breakfact
+                "breakfast" -> Breakfast
                 "lunch" -> Lunch
                 "dinner" -> Dinner
-                else -> Breakfact
+                else -> Breakfast
             }
         }
     }
