1bf16f6abc992e84c3b3ed9d11aa09c510b13038
Darya Kovalchuk <daryakovalchuk167@gmail.com>
2024-04-14 00:41:17 +0300

    add expandable meal ui

--- tracker/tracker_presentation/src/main/java/com/plcoding/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/plcoding/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,14 +1,20 @@
 package com.plcoding.tracker_presentation.tracker_overview
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.plcoding.core.util.UiEvent
 import com.plcoding.core_ui.LocalSpacing
+import com.plcoding.tracker_presentation.tracker_overview.components.DaySelector
+import com.plcoding.tracker_presentation.tracker_overview.components.ExpandableMeal
 import com.plcoding.tracker_presentation.tracker_overview.components.NutrientsHeader
 
 @Composable
@@ -26,6 +32,33 @@ fun TrackerOverviewScreen(
     ) {
         item {
             NutrientsHeader(state = state)
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(
+                date = state.date,
+                onPreviousDayClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnPreviousDayClick)
+                },
+                onNextDayClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnNextDayClick)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
+
+        items(state.meals) { meal ->
+            ExpandableMeal(
+                meal = meal,
+                onToggleClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnToggleMealClick(meal))
+                },
+                content = {
+
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
